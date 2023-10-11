1b5376997507619d96433dafdf8fa8cc05c715f2
Raslan Ribeiro <raslan.ribeiro@cortex-intelligence.com>
2023-10-11 15:23:54 -0300

    Added data and selectivity factor

--- utils/manipulating_data.py
+++ utils/manipulating_data.py
@@ -10,7 +10,7 @@ def add_new_columns():
     return pd.DataFrame(data, columns=['Name', 'runtime_range'])
 
 def columns_to_be_used_as_input():
-    return ["input_rows_quantity", "input_columns_quantity","output_columns_quantity","number_of_workers", "photon_acceleration", "constraint","cte","case_when","inner_join","left_join","right_join","group_by", "where","and","or","subquery", "explode", "create", "read", "update", "delete", "vcpu", "memory_ram_gb", "instance_storage_type"]
+    return ["input_rows_quantity", "input_columns_quantity","output_columns_quantity","number_of_workers", "photon_acceleration", "constraint","cte","case_when","inner_join","left_join","right_join","group_by", "selectivity_factor","subquery", "explode", "create", "read", "update", "delete", "vcpu", "memory_ram_gb", "instance_storage_type"]
 
 # ter um fator de seletividade - pegar da estatística do banco -> só tem quantidade de bytes nas estatisticas
 # databricks: analyse table - compute statistics -> mesma respota da pergunta acima
@@ -26,16 +26,16 @@ def column_to_be_used_as_output():
 def get_cleaned_data(columns_to_be_used_as_input, column_to_be_used_as_output):
 
     # INPUT DATA
-    executions = pd.read_csv("data/input/receita_federal_executions.csv",thousands=',')
-    operations = pd.read_csv("data/input/receita_federal _operations.csv", thousands=',')
-    instances = pd.read_csv("data/input/receita_federal_instances.csv", thousands=',')
+    executions = pd.read_csv("data/input/query_executions.csv",thousands=',')
+    operations = pd.read_csv("data/input/query _operations.csv", thousands=',')
+    instances = pd.read_csv("data/input/query_instances.csv", thousands=',')
 
     # QUERY
     df = executions.merge(instances, left_on='worker_type', right_on='instance_type', how="left").merge(operations, on='query_name', how="left")
     df = df[columns_to_be_used_as_input + column_to_be_used_as_output]
 
     # FORMAT
-    df = df.astype({"input_rows_quantity":"int","input_columns_quantity":"int","output_columns_quantity":"int","number_of_workers":"int","constraint":"int","cte":"int","case_when":"int","inner_join":"int","left_join":"int","right_join":"int","group_by":"int","where":"int","and":"int","or":"int","subquery":"int", "vcpu":"int", "memory_ram_gb":"int"})
+    df = df.astype({"input_rows_quantity":"int","input_columns_quantity":"int","output_columns_quantity":"int","number_of_workers":"int","constraint":"int","cte":"int","case_when":"int","inner_join":"int","left_join":"int","right_join":"int","group_by":"int","selectivity_factor":"float","subquery":"int", "vcpu":"int", "memory_ram_gb":"int"})
 
     le = preprocessing.LabelEncoder()
     df['instance_storage_type'] = le.fit_transform(df['instance_storage_type'])
