704e2f51feab1ef5e148b428aac8c7e958330365
Raslan Ribeiro <raslan.ribeiro@cortex-intelligence.com>
2023-11-18 22:57:58 -0300

    Cleaning files

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestRegressor
-from sklearn import tree
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
@@ -16,7 +15,6 @@ x = df[columns_to_be_used_as_input]
 y = df[column_to_be_used_as_output]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
 
-# Linear regression
 print("LINEAR MODEL")
 model = LinearRegression()
 model.fit(x_train, y_train.values.ravel())
@@ -24,9 +22,8 @@ y_pred = model.predict(x_test)
 print("Accuracy: ", metrics.accuracy_score(y_test, np.round(y_pred)))
 print("F1 score: ", metrics.f1_score(y_test, np.round(y_pred), average="weighted"))
 
-print("\n------------------------------------\n")
+print("\n")
 
-# Logistic regression
 print("LOGISTIC MODEL")
 model = LogisticRegression()
 model.fit(x_train, y_train.values.ravel())
@@ -34,30 +31,23 @@ y_pred = model.predict(x_test)
 print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
 print("F1 score: ", metrics.f1_score(y_test, np.round(y_pred), average="weighted"))
 
-print("\n------------------------------------\n")
+print("\n")
 
-# Random forest
+print("RANDOM FOREST")
 model = RandomForestRegressor(random_state=42)
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 print("Accuracy: ", metrics.accuracy_score(y_test, np.round(y_pred)))
 print("F1 score: ", metrics.f1_score(y_test, np.round(y_pred), average="weighted"))
 
+print("\n")
 
-print("\n------------------------------------\n")
-
-# Decision tree
 print("DECISION TREE")
 model = DecisionTreeClassifier(random_state=0)
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 print("Accuracy:", metrics.accuracy_score( y_test, y_pred))
 print("F1 score: ", metrics.f1_score(y_test, y_pred, average="weighted"))
-
-# Graph
-# plt.figure(figsize=(12,12))
-# tree.plot_tree(model, fontsize=10, feature_names=columns_to_be_used_as_input, class_names=df['runtime_range'], filled=True, proportion=True)
-# plt.show()
 
 # Feature importance
 importance = model.feature_importances_
@@ -89,33 +79,21 @@ from sklearn import preprocessing
 from sklearn.decomposition import PCA #used to reduce dimension
 import pandas as pd
 
-k = 6
-
 x_norm = preprocessing.normalize(x)
 
-model = KMeans(n_clusters = k, random_state = 0, n_init='auto').fit(x_norm)
+model = KMeans(n_clusters = 6, random_state = 0, n_init='auto').fit(x_norm)
 
 pca = PCA(n_components=2) 
 df_2d = pca.fit_transform(x_norm)
 df_new = pd.DataFrame(df_2d, columns=['PC1', 'PC2'])
 df_new['Cluster'] = model.labels_
 
-sb.scatterplot(x="PC1", y="PC2", data = df_new, hue = "Cluster", palette='viridis')
-plt.title(f"K-Means Clustering (k={k})")
-plt.show()
-
-
-# ---------------------------------
-# Create an empty list to store the WCSS values
 wcss = []
 
-# Specify a range of k values to try
-k_range = range(1, 11)  # You can adjust the upper limit of k as needed
-
-# Fit K-Means models for each k and calculate WCSS
+k_range = range(1, 11)
 for k in k_range:
     kmeans = KMeans(n_clusters=k, random_state=42)
-    kmeans.fit(df_new)  # Replace 'data' with your dataset
+    kmeans.fit(df_new)
     wcss.append(kmeans.inertia_)
 
 plt.figure(figsize=(8, 6))
